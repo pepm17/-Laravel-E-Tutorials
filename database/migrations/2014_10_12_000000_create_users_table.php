bdfd2a92fd963ad7ec4f686a0b7753fe772a5061
Pablo Emilio <pablopupo1797@gmail.com>
2019-03-07 22:57:29 -0500

    Revert "Database"
    This reverts commit a32ee6bd07b143e0cca6489c54045e91517dc13e.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,6 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('identification');
-            $table->boolean('isTutor');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
